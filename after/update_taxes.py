e46687b76690020f2ce12388bc824294fce8c084
Raimon Esteve <raimon@nan-tic.com>
2018-12-24 12:26:04 +0100

    Set company when update taxes

--- after/update_taxes.py
+++ after/update_taxes.py
@@ -39,74 +39,85 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-with Transaction().start(dbname, 0, context=context) as transaction:
+with Transaction().start(dbname, 1, context=context) as transaction:
     Module = pool.get('ir.module')
+    Company = pool.get('company.company')
 
     cursor = Transaction().connection.cursor()
-    cursor.execute('select * from mapping_taxes')
 
-    xml_ids = {}
-    parent_map = {}
-    for x in cursor.fetchall():
-        tax_id, name, parent, i347, fs_id, template_id, xml_id = x
-        if '_' == xml_id[-1]:
-            xml_id = xml_id[:-1]
+    domain = []
+    child_companies = Company.search([('parent', '!=', None)])
+    if child_companies:
+        domain.append(('parent', '!=', None))
 
-        # print(xml_id, name, fs_id)
-        new_template, new_tax = get_tax(xml_id)
+    for company in Company.search(domain):
+        logger.info("company %s" % company.id)
+        with Transaction().set_context(company=company.id):
+            cursor.execute('select * from mapping_taxes')
 
-        if xml_id in xml_ids:
-            taxes, nt = xml_ids[xml_id]
-            taxes.append(str(tax_id))
-            xml_ids[xml_id] = (taxes, nt)
-        else:
-            xml_ids[xml_id] = ([str(tax_id)], new_tax.id)
+            xml_ids = {}
+            parent_map = {}
+            for x in cursor.fetchall():
+                tax_id, name, parent, i347, fs_id, template_id, xml_id = x
+                if '_' == xml_id[-1]:
+                    xml_id = xml_id[:-1]
 
-        if parent in parent_map:
-            parent_map[parent].append(new_tax.id)
-        else:
-            parent_map[parent] = [new_tax.id]
+                # print(xml_id, name, fs_id)
+                new_template, new_tax = get_tax(xml_id)
 
-    tables = [
-        ('account_invoice_tax', 'tax'),
-        ('account_tax_line', 'tax')
-    ]
-    # print(xml_ids)
-    for x in xml_ids:
-        taxes, new_tax = xml_ids[x]
-        for table, field in tables:
-            cursor.execute(
-                'update %s set %s = %s where tax in (%s)' % (
-                    table, field, new_tax, ",".join(taxes)
-                )
-            )
+                if xml_id in xml_ids:
+                    taxes, nt = xml_ids[xml_id]
+                    taxes.append(str(tax_id))
+                    xml_ids[xml_id] = (taxes, nt)
+                else:
+                    xml_ids[xml_id] = ([str(tax_id)], new_tax.id)
 
-    Transaction().connection.commit()
+                if parent in parent_map:
+                    parent_map[parent].append(new_tax.id)
+                else:
+                    parent_map[parent] = [new_tax.id]
 
-    tables2 = [('account_invoice_line_account_tax', 'tax')]
-    sales = Module.search([
-        ('name', '=', 'sale'),
-        ('state', '=', 'activated')], limit=1)
-    if sales:
-        tables2.append(('sale_line_account_tax', 'tax'))
-    purchases = Module.search([
-        ('name', '=', 'purchase'),
-        ('state', '=', 'activated')], limit=1)
-    if purchases:
-        tables2.append(('purchase_line_account_tax', 'tax'))
+            tables = [
+                ('account_invoice_tax', 'tax'),
+                ('account_tax_line', 'tax')
+            ]
+            # print(xml_ids)
+            for x in xml_ids:
+                taxes, new_tax = xml_ids[x]
+                for table, field in tables:
+                    cursor.execute(
+                        'update %s set %s = %s where tax in (%s)' % (
+                            table, field, new_tax, ",".join(taxes)
+                        )
+                    )
 
-    for parent, taxes in parent_map.items():
-        for table, field in tables2:
-            cursor.execute('select id, line from %s where %s = %s' % (
-                table, field, parent))
+            Transaction().connection.commit()
 
-            for id_, line in cursor.fetchall():
-                # cursor.execute('update %s set %s=%s where id=%s' % (
-                #    table, field, taxes[0], id_))
-                cursor.execute('delete from %s where id=%s'%(table, id_))
-                for tax in taxes:
-                    cursor.execute('insert into %s(tax,line) values(%s,%s)' % (
-                        table, tax, line
-                    ))
+            tables2 = [('account_invoice_line_account_tax', 'tax')]
+            sales = Module.search([
+                ('name', '=', 'sale'),
+                ('state', '=', 'activated')], limit=1)
+            if sales:
+                tables2.append(('sale_line_account_tax', 'tax'))
+            purchases = Module.search([
+                ('name', '=', 'purchase'),
+                ('state', '=', 'activated')], limit=1)
+            if purchases:
+                tables2.append(('purchase_line_account_tax', 'tax'))
 
-    Transaction().connection.commit()
+            for parent, taxes in parent_map.items():
+                for table, field in tables2:
+                    cursor.execute('select id, line from %s where %s = %s' % (
+                        table, field, parent))
+
+                    for id_, line in cursor.fetchall():
+                        # cursor.execute('update %s set %s=%s where id=%s' % (
+                        #    table, field, taxes[0], id_))
+                        cursor.execute('delete from %s where id=%s'%(table, id_))
+                        for tax in taxes:
+                            cursor.execute('insert into %s(tax,line) values(%s,%s)' % (
+                                table, tax, line
+                            ))
+
+            Transaction().connection.commit()
+
