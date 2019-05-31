360c2237dc1161137eb1b8ad2e772f9b0309996f
Raimon Esteve <raimon@nan-tic.com>
2019-05-31 11:23:44 +0200

    Not add new taxes in case the tax is current in parent map (after/update_taxes.py)

--- after/update_taxes.py
+++ after/update_taxes.py
@@ -97,7 +97,8 @@ with Transaction().start(dbname, 1, context=context) as transaction:
                     template_map[parent_template] = [new_template.id]
 
                 if parent in parent_map:
-                    parent_map[parent].append((new_tax.id, new_template.id))
+                    if not (new_tax.id, new_template.id) in parent_map[parent]:
+                        parent_map[parent].append((new_tax.id, new_template.id))
                 else:
                     parent_map[parent] = [(new_tax.id, new_template.id)]
 
