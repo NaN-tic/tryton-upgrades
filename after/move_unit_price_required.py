97fb190acbfce937ae35684a9de430de15ac05de
Raimon Esteve <raimonesteve@gmail.com>
2025-04-24 07:27:26 +0200

    Add move_unit_price_required in after section

--- after/move_unit_price_required.py
+++ after/move_unit_price_required.py
@@ -1,14 +1,44 @@
-Move = pool.get('stock.move')
+#!/usr/bin/env python
+import sys
 
-moves = Move.search([])
-ids = []
-for move in moves:
-    if not move.unit_price_required:
-        ids.append(str(move.id))
+dbname = sys.argv[1]
+config_file = sys.argv[2]
 
-query = 'update stock_move set unit_price = null, currency=null where id in (%s)' % ','.join(ids)
-print(query)
+from trytond.config import config as CONFIG
+CONFIG.update_etc(config_file)
 
-cursor = transaction.connection.cursor()
-cursor.execute(query)
-transaction.commit()
+from trytond.pool import Pool
+from trytond.transaction import Transaction
+import logging
+
+Pool.start()
+pool = Pool(dbname)
+pool.init()
+
+context = {}
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+with Transaction().start(dbname, 0, context=context) as transaction:
+    Move = pool.get('stock.move')
+
+    ids = []
+    moves = Move.search([
+        ('state', 'not in', ['done', 'cancelled']), # TODO remove domain in case user do copy oldest shipments
+        ])
+    for move in moves:
+        if not move.unit_price_required:
+            ids.append(str(move.id))
+
+    query = 'update stock_move set unit_price = null, currency=null where id in (%s)' % ','.join(ids)
+    print(query)
+
+    cursor = transaction.connection.cursor()
+    cursor.execute(query)
+    transaction.commit()
